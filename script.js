46ff2a434cc112c6736f91c6331f72266298986c
prachi2399 <74529750+prachi2399@users.noreply.github.com>
2020-12-24 11:30:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-// script.js
 
-// Custom function
 function calculateTip() {
     
-    // Store the data of inputs
     var billAmount = document.getElementById("billAmount").value;
     var serviceQuality = document.getElementById("serviceQuality").value;
     var numPeople = document.getElementById("totalPeople").value;
     
-    // Quick validation
     if(billAmount === "" || serviceQuality == 0) {
         window.alert("Please enter some values to get this baby up and running!");
         return; // this will prevent the function from continuing
     }
     
-    // Check to see if this input is empty or less than or equal to 1
     if(numPeople === "" || numPeople <= 1) {
         numPeople = 1;
         
@@ -26,23 +21,20 @@ function calculateTip() {
         
     }
     
-    // Do some math!
     var total = (billAmount * serviceQuality) / numPeople;
     total = Math.round(total * 100) / 100;
     total = total.toFixed(2);
     
     
-    // Display the tip!
     document.getElementById("totalTip").style.display = "block";
     document.getElementById("tip").innerHTML = total;
     
 }
 
-// Hide the tip amount on load
 document.getElementById("totalTip").style.display = "none";
 document.getElementById("each").style.display = "none";
 
-// Clicking the button calls our custom function
+
 document.getElementById("calculate").onclick = function() { calculateTip(); };
 
 
